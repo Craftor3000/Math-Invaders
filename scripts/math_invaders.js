7678fd3cafe7a7a2a24c3c1bbec1f46c8e2f893d
Craftor3000 <craftor3000@gmail.com>
2023-12-14 21:47:16 +0100

    Vaisseaux (alpha 2)

--- scripts/math_invaders.js
+++ scripts/math_invaders.js
@@ -13,14 +13,14 @@ function debut_jeu(){
 	if (!jeu_en_cours) {
 		jeu_en_cours = true;
 		texte_commencer.textContent = "Phase 1";
+		texte_banniere.textContent = "Phase 1";
 		grp_gauche.style.visibility = "visible";
 		grp_centre.style.visibility = "visible";
 		grp_droite.style.visibility = "visible";		
 		joueur.style.visibility = "visible";
-		for (var i = 0; i <= 3; i++) {
-			for (var j = 0; i < 2; i++) {
-				operations[i][j] = nb_random();
-			};
+		for (var i = 0; i < 3; i++) {
+			operations[i][0] = nb_random();
+			operations[i][1] = nb_random();
 		};
 		console.log(operations)
 		affiche_operation();
@@ -31,6 +31,7 @@ function fin_jeu() {
 	if (jeu_en_cours) {
 		jeu_en_cours = false
 		texte_commencer.textContent = "Commencer !";
+		texte_banniere.textContent = "Appuyez pour commencer !";
 		grp_gauche.style.visibility = "hidden";
 		grp_centre.style.visibility = "hidden";
 		grp_droite.style.visibility = "hidden";
@@ -75,6 +76,8 @@ const jeu = document.querySelector("#jeu");
 const parametres = document.querySelector("#parametres");
 const commencer = document.querySelector("#commencer");
 const texte_commencer = document.querySelector("#texte_commencer");
+const texte_banniere = document.querySelector("#texte_banniere");
+const proprietes = document.querySelector("#proprietes");
 
 const f_regles = document.querySelector("#f_regles");
 const f_jeu = document.querySelector("#f_jeu");
