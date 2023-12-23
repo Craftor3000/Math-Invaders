cb498e6abb11c65b03ac3c1eb59e16182798db71
Maximilien <maximilien.braley@gmail.com>
2023-12-23 15:38:54 +0100

    Tentative rattrapage

--- scripts/math_invaders.js
+++ scripts/math_invaders.js
@@ -1,18 +1,29 @@
 /* FONCTIONS */
 
-function de() {
-   return Math.floor(Math.random() * 10 + 1);
+function nb_random() {
+	return Math.floor(Math.random() * 10 + 1);
+}
+
+function random_boss() {
+	return Math.floor(Math.random() * 20 + 1);
+
 }
 
 function debut_jeu(){
 	if (!jeu_en_cours) {
 		jeu_en_cours = true;
 		texte_commencer.textContent = "Phase 1";
-		ennemis.style.visibility = "visible";
+		texte_banniere.textContent = "Phase 1";
+		grp_gauche.style.visibility = "visible";
+		grp_centre.style.visibility = "visible";
+		grp_droite.style.visibility = "visible";		
 		joueur.style.visibility = "visible";
-		espace_operation.style.visibility = "visible";
-		input_j.style.visibility = "visible";
-		op_g.textContent = assigne_operation_1()
+		for (var i = 0; i < 3; i++) {
+			operations[i][0] = nb_random();
+			operations[i][1] = nb_random();
+		};
+		console.log(operations)
+		affiche_operation();
 	};
 };
 
@@ -20,10 +31,11 @@ function fin_jeu() {
 	if (jeu_en_cours) {
 		jeu_en_cours = false
 		texte_commencer.textContent = "Commencer !";
-		ennemis.style.visibility = "hidden";
+		texte_banniere.textContent = "Appuyez pour commencer !";
+		grp_gauche.style.visibility = "hidden";
+		grp_centre.style.visibility = "hidden";
+		grp_droite.style.visibility = "hidden";
 		joueur.style.visibility = "hidden";
-		espace_operation.style.visibility = "hidden";
-		input_j.style.visibility = "hidden";
 	};
 };
 
@@ -38,10 +50,10 @@ function affiche_jeu() {
 	f_jeu.style.visibility = "visible";
 	f_parametres.style.visibility = "hidden";
 	if (!jeu_en_cours) {
-		ennemis.style.visibility = "hidden";
+		grp_gauche.style.visibility = "hidden";
+		grp_centre.style.visibility = "hidden";
+		grp_droite.style.visibility = "hidden";
 		joueur.style.visibility = "hidden";
-		espace_operation.style.visibility = "hidden";
-		input_j.style.visibility = "hidden";
 	};	
 };
 function affiche_parametres() {
@@ -51,12 +63,10 @@ function affiche_parametres() {
 	fin_jeu();
 };
 
-function assigne_operation_1() {
-	let a = de();
-	let b = de();
-	resulat = a * b
-	return (a).toString() + ' x ' + (b).toString() + ' = ?';
-
+function affiche_operation() {
+	droite.textContent = operations[0][0].toString() + " x " + operations[0][1].toString();
+	centre.textContent = operations[1][0].toString() + " x " + operations[1][1].toString();
+	gauche.textContent = operations[2][0].toString() + " x " + operations[2][1].toString();
 }
 
 /* MAIN */
@@ -66,6 +76,8 @@ const jeu = document.querySelector("#jeu");
 const parametres = document.querySelector("#parametres");
 const commencer = document.querySelector("#commencer");
 const texte_commencer = document.querySelector("#texte_commencer");
+const texte_banniere = document.querySelector("#texte_banniere");
+const proprietes = document.querySelector("#proprietes");
 
 const f_regles = document.querySelector("#f_regles");
 const f_jeu = document.querySelector("#f_jeu");
@@ -73,14 +85,24 @@ const f_parametres = document.querySelector("#f_parametres");
 const espace = document.querySelector("#espace_jeu");
 
 const joueur = document.querySelector("#joueur");
-const ennemis = document.querySelector("#ennemis");
 const ennemi_gauche = document.querySelector("#ennemi_gauche");
 const ennemi_centre = document.querySelector("#ennemi_centre");
 const ennemi_droite = document.querySelector("#ennemi_droite");
+const grp_gauche = document.querySelector("#grp_gauche");
+const grp_centre = document.querySelector("#grp_centre");
+const grp_droite = document.querySelector("#grp_droite");
+const gauche = document.querySelector("#operation_gauche");
+const centre = document.querySelector("#operation_centre");
+const droite = document.querySelector("#operation_droite");
+
+
+
+
 const boss = document.querySelector("#boss");
 
 var jeu_en_cours = false;
-var resulat = 0
+var operations = [[0,0],[0,0],[0,0]];
+
 
 regles.addEventListener("click", affiche_regles);
 jeu.addEventListener("click", affiche_jeu);
