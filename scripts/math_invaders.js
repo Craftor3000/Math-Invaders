877bc80f31d5bcd7a0469a7e15a32ab9f10f5a43
Craftor3000 <craftor3000@gmail.com>
2023-12-27 13:23:58 +0100

    Boss - 1

--- scripts/math_invaders.js
+++ scripts/math_invaders.js
@@ -15,7 +15,6 @@ function timer(t) {
 function debut_jeu(){
 	if (!jeu_en_cours) {
 		joueur.style.visibility = "visible";
-		score.style.visibility = "visible";
 		jeu_en_cours = true;
 		main_jeu();
 	};
@@ -26,11 +25,17 @@ function fin_jeu() {
 		jeu_en_cours = false
 		texte_commencer.textContent = "Commencer !";
 		texte_banniere.textContent = "Appuyez pour commencer !";
+		grp_boss.style.visibility = "hidden";
+		boss.style.visibility = "hidden";
+		boss_1.style.visibility = "hidden";
+		boss_2.style.visibility = "hidden";
+		boss_3.style.visibility = "hidden";
+		boss_4.style.visibility = "hidden";
+		boss_5.style.visibility = "hidden";
 		grp_gauche.style.visibility = "hidden";
 		grp_centre.style.visibility = "hidden";
 		grp_droite.style.visibility = "hidden";
 		joueur.style.visibility = "hidden";
-		score.style.visibility = "hidden";
 		grp_gauche.style.border = "0px";
 		grp_centre.style.border = "0px";
 		grp_droite.style.border = "0px";
@@ -41,32 +46,50 @@ function fin_jeu() {
 		phase = 0;
 		resolutions = 3;
 		score.textContent = "0";
-		ajout_score = 0;
 	};
 };
 
 
 async function main_jeu() {
 	while(jeu_en_cours) {
+
 		if(resolutions == 3) {
-			affichage_operations();
-			apparition_vaisseaux();
+
 			phases();
-			ajout_score += 100;
 			resolutions = 0;
 			avancement = 0;
+			temps_vaisseau = 0;
+
+			if (phase == 1) {
+				boss_en_cours = true;
+				affichage_operations_boss();
+				apparition_boss();
+
+			} else {
+				affichage_operations();
+				apparition_vaisseaux();
+			}
 		}
-		if (!vaisseau_choisit) {
+
+		if (!vaisseau_choisit && !boss_en_cours) {
 			selection_vaisseau();
 		}
+
+		limite_avancement = espace.offsetHeight - (espace.offsetHeight / 10);
 		avancement_vaisseaux();
 		await timer(vitesse);
 
-		if (avancement == 300) {
+		if (avancement == limite_avancement) {
 			fin_jeu();
 			break;
 		}
 	}
+}
+
+function phases() {
+	phase++;
+	texte_banniere.textContent = "Phase " + phase.toString();
+	vitesse /= 1.5;
 }
 
 function apparition_vaisseaux() {
@@ -75,10 +98,14 @@ function apparition_vaisseaux() {
 	grp_droite.style.visibility = "visible";
 }
 
-function phases() {
-	phase++;
-	texte_banniere.textContent = "Phase " + phase.toString();
-	vitesse /= 1.5;
+function apparition_boss() {
+	grp_boss.style.visibility = "visible";
+	boss.style.visibility = "visible";
+	boss_1.style.visibility = "visible";
+	boss_2.style.visibility = "visible";
+	boss_3.style.visibility = "visible";
+	boss_4.style.visibility = "visible";
+	boss_5.style.visibility = "visible";
 }
 
 function selection_vaisseau() {
@@ -93,14 +120,14 @@ function selection_vaisseau() {
 	}
 }
 
-function valide() {
-	// ENVOI DU MISSILE
+function validation() {
 	if (reponse_joueur.valueAsNumber == operations[vaisseau_choisit][0] * operations[vaisseau_choisit][1]) {
 		elimination();
-		score.textContent = parseInt(score.textContent) + score_avec_avancement();
+		score.textContent = parseInt(score.textContent) + calcul_score();
+		temps_vaisseau = 0;
 	}
 	else {
-		score.textContent = parseInt(score.textContent) - ajout_score;
+		score.textContent = parseInt(score.textContent) - calcul_score();
 		if (parseInt(score.textContent) < 0){
 			score.textContent = 0;
 		}
@@ -108,9 +135,17 @@ function valide() {
 	reponse_joueur.valueAsNumber = NaN;
 }
 
-function score_avec_avancement() {
-	let pourcentage_tps_restant = (300 - avancement) / 300;
-	return Math.round((ajout_score * pourcentage_tps_restant)/10) * 10;
+function elimination() {
+	vaisseaus_vivants[vaisseau_choisit] = false;
+	groupes[proposition_vaisseau].style.border = "0px";
+	groupes[vaisseau_choisit].style.visibility = "hidden";
+	vaisseau_choisit = null;
+	resolutions++;
+}
+
+function calcul_score() {
+	let pourcentage_tps_restant = (limite_avancement - temps_vaisseau) / limite_avancement;
+	return Math.round(((phase * 100) * pourcentage_tps_restant) / 10) * 10;
 }
 
 
@@ -125,44 +160,43 @@ function affichage_operations() {
 	vaisseaus_vivants = [true, true, true];
 };
 
+function affichage_operations_boss() {
+	for (var i = 0; i < 5; i++) {
+		operations_boss[i][0] = nb_random(10);
+		operations_boss[i][1] = nb_random(10);
+	};
+	boss_1.textContent = operations_boss[0][0].toString() + " x " + operations_boss[0][1].toString();
+	boss_2.textContent = operations_boss[1][0].toString() + " x " + operations_boss[1][1].toString();
+	boss_3.textContent = operations_boss[2][0].toString() + " x " + operations_boss[2][1].toString();
+	boss_4.textContent = operations_boss[3][0].toString() + " x " + operations_boss[3][1].toString();
+	boss_5.textContent = operations_boss[4][0].toString() + " x " + operations_boss[4][1].toString();
+}
+
 function avancement_vaisseaux() {
-	avancement += 1;
+	avancement++;
+	temps_vaisseau++;
 	grp_gauche.style.margin = avancement.toString() + "px 0px 0px 0px";
 	grp_centre.style.margin = avancement.toString() + "px 0px 0px 0px";
 	grp_droite.style.margin = avancement.toString() + "px 0px 0px 0px";
+	grp_boss.style.margin = avancement.toString() + "px 0px 0px 0px";
 };
 
-function elimination() {
-	vaisseaus_vivants[vaisseau_choisit] = false;
-	groupes[proposition_vaisseau].style.border = "0px";
-	groupes[vaisseau_choisit].style.visibility = "hidden";
-	vaisseau_choisit = null;
-	resolutions++;
-}
+
 
 function affiche_regles() {
 	f_regles.style.visibility = "visible";
 	f_jeu.style.visibility = "hidden";
-	score.style.visibility = "hidden";
 	f_parametres.style.visibility = "hidden";
 	fin_jeu();
 };
 function affiche_jeu() {
 	f_regles.style.visibility = "hidden";
 	f_jeu.style.visibility = "visible";
-	score.style.visibility = "visible";
 	f_parametres.style.visibility = "hidden";
-	if (!jeu_en_cours) {
-		grp_gauche.style.visibility = "hidden";
-		grp_centre.style.visibility = "hidden";
-		grp_droite.style.visibility = "hidden";
-		joueur.style.visibility = "hidden";
-	};	
 };
 function affiche_parametres() {
 	f_regles.style.visibility = "hidden";
 	f_jeu.style.visibility = "hidden";
-	score.style.visibility = "hidden";
 	f_parametres.style.visibility = "visible";
 	fin_jeu();
 };
@@ -180,6 +214,7 @@ const texte_banniere = document.querySelector("#texte_banniere");
 const proprietes = document.querySelector("#proprietes");
 const score = document.querySelector("#score_joueur");
 const reponse_joueur = document.querySelector("#entree_resultat");
+const valider = document.querySelector("#valider");
 
 const f_regles = document.querySelector("#f_regles");
 const f_jeu = document.querySelector("#f_jeu");
@@ -197,18 +232,30 @@ const groupes = [grp_gauche, grp_centre, grp_droite];
 const gauche = document.querySelector("#operation_gauche");
 const centre = document.querySelector("#operation_centre");
 const droite = document.querySelector("#operation_droite");
+
 const boss = document.querySelector("#boss");
+const grp_boss = document.querySelector("#grp_boss");
+const boss_1 = document.querySelector("#boss_1");
+const boss_2 = document.querySelector("#boss_2");
+const boss_3 = document.querySelector("#boss_3");
+const boss_4 = document.querySelector("#boss_4");
+const boss_5 = document.querySelector("#boss_5");
+const groupes_boss = [boss_1, boss_2, boss_3, boss_4, boss_5];
 
 var jeu_en_cours = false;
+var boss_en_cours = false;
 var phase = 0;
 var vitesse = 1000;
 var operations = [[0,0],[0,0],[0,0]];
+var operations_boss = [[0,0],[0,0],[0,0],[0,0],[0,0]]
 var avancement = 0;
+var temps_vaisseau = 0;
+var limite_avancement = espace.offsetHeight - (espace.offsetHeight / 10);
 var resolutions = 3;
 var vaisseau_choisit = null;
 var vaisseaus_vivants = [false, false, false];
+var boss_vivant = [false, false, false, false, false];
 var proposition_vaisseau = 0;
-var ajout_score = 0;
 
 
 
@@ -216,10 +263,10 @@ regles.addEventListener("click", affiche_regles);
 jeu.addEventListener("click", affiche_jeu);
 parametres.addEventListener("click", affiche_parametres);
 commencer.addEventListener("click", debut_jeu);
+valider.addEventListener("click", validation)
 
 reponse_joueur.addEventListener("keydown", (e) => {
-	console.log(e.key);
 	if (e.key == "Enter") {
-		valide();
+		validation();
 	};
 });
